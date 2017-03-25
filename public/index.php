b97637ff53a36832d9fcfc487e46916944b1bbfd
claudinei <kostaney@hotmail.com>
2017-03-25 20:41:43 -0300

    index alterado para primeiro exercio listar post

--- public/index.php
+++ public/index.php
@@ -1,10 +1,36 @@
 <?php
 require_once __DIR__.'/../bootstrap.php';
 
+$posts = array(
+    array('id'=>1,'conteudo'=>'primeiro conteudo Nam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla'),
+    array('id'=>2,'conteudo'=>'segundo conteudo Nam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla'),
+    array('id'=>3,'conteudo'=>'terceiro postNam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla'),
+    array('id'=>4,'conteudo'=>'quarto postNam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla'),
+    array('id'=>5,'conteudo'=>'quinto postNam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla'),
+    array('id'=>6,'conteudo'=>'sexto postNam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla'),
+    array('id'=>7,'conteudo'=>'setimo postNam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla'),
+    array('id'=>8,'conteudo'=>'oitavo postNam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla'),
+    array('id'=>9,'conteudo'=>'nono postNam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla'),
+    array('id'=>10,'conteudo'=>'descimo postNam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla')
+);
 $app->get('/',function() use($response) {
 
-    $response->setContent('Eu ainda estou aqyu');
+    $response->setContent('home');
     return $response;
 });
+$app->get('/post',function() use($response,$posts,$app){
+    return $app->json($posts);
+})
+    ->bind('lita_posts');
 
+$app->get('/post/{id}',function($id) use($response,$posts,$app){
+    $key = array_search($id, array_column($posts, 'id'));
+    if ($key!==0 && !$key) return $app->abort(401,"O id {$id} não é válido!");
+    return $app->json($posts[$key]);
+})
+    ->bind('get_post')
+    ->assert('id','\d+');
+
+
+//  ultima linha ***** antes de executar o programa
 $app->run();
